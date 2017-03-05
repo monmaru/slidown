73e35b0f233e609751dcd18993667bc6e4daa328
Yoshitomo Minami <minami530@gmail.com>
2017-03-05 18:53:59 +0900

    Refactoring

--- handler.go
+++ handler.go
@@ -9,23 +9,50 @@ import (
 	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 )
 
-func DownloadFromSlideShare(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	data, err := json2ReqData(r.Body)
-	if err != nil {
-		log.Debugf(ctx, "Error json2Data : %v", err)
-		writeErrorResponse(w, "Invalid request format!!", http.StatusBadRequest)
-		return
-	}
+type HandlerWithContext func(context.Context, http.ResponseWriter, *http.Request)
 
+func HttpWithContext(h HandlerWithContext) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := NewContext(r)
+		defer timeTrack(ctx, time.Now(), r.RequestURI)
+		h(ctx, w, r)
+	})
+}
+
+var NewContext func(*http.Request) context.Context = func(r *http.Request) context.Context {
+	return appengine.NewContext(r)
+}
+
+func timeTrack(ctx context.Context, start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Debugf(ctx, "%s took %s", name, elapsed)
+}
+
+type HandlerWithReqData func(context.Context, http.ResponseWriter, *ReqData)
+
+func MustParams(h HandlerWithReqData) HandlerWithContext {
+	return HandlerWithContext(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		data, err := json2ReqData(r.Body)
+		if err != nil || data.URL == "" {
+			log.Debugf(ctx, "Error json2Data : %v", err)
+			writeError(w, "Invalid request format!!", http.StatusBadRequest)
+			return
+		}
+		h(ctx, w, data)
+	})
+}
+
+func DownloadFromSlideShare(ctx context.Context, w http.ResponseWriter, data *ReqData) {
 	svc := createSlideShareSvc(ctx)
 	slide, err := svc.GetSlideShareInfo(data.URL)
 	if err != nil {
-		log.Debugf(ctx, "GetSlideShareInfo error: %#v", err)
-		writeErrorResponse(w, "スライドが見つかりませんでした。", http.StatusNotFound)
+		log.Infof(ctx, "GetSlideShareInfo error: %#v", err)
+		writeError(w, "スライドが見つかりませんでした。", http.StatusNotFound)
 		return
 	}
 
@@ -33,22 +60,18 @@ func DownloadFromSlideShare(ctx context.Context, w http.ResponseWriter, r *http.
 		resp, err := download(ctx, slide.DownloadURL)
 		if err != nil {
 			log.Errorf(ctx, "download error: %#v", err)
-			writeErrorResponse(w, "ダウンロード中にエラーが発生しました。", http.StatusInternalServerError)
+			writeError(w, "ダウンロード中にエラーが発生しました。", http.StatusInternalServerError)
 			return
 		}
 		defer resp.Body.Close()
 
 		fileName := fmt.Sprint(slide.ID) + "." + slide.Format
-		w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
-		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-		w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
-		w.Header().Set("X-FileName", fileName)
-		io.Copy(w, resp.Body)
+		copyResponseAsFile(w, resp, fileName)
 	} else {
 		// TODO: ダウンロード不可のスライドの場合は、画像ファイルをもとにPDFを作成して返す。
 		msg := "指定されたスライドはダウンロード禁止です。"
-		log.Debugf(ctx, msg)
-		writeErrorResponse(w, msg, http.StatusBadRequest)
+		log.Infof(ctx, msg)
+		writeError(w, msg, http.StatusBadRequest)
 	}
 }
 
@@ -56,41 +79,30 @@ func createSlideShareSvc(ctx context.Context) *SlideShareSvc {
 	return NewSlideShareSvc(
 		os.Getenv("APIKEY"),
 		os.Getenv("SHAREDSECRET"),
-		GetHTTPClient(ctx))
+		NewHTTPClient(ctx))
 }
 
-func DownloadFromSpeakerDeck(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	data, err := json2ReqData(r.Body)
-	if err != nil {
-		log.Debugf(ctx, "Error json2Data : %v", err)
-		writeErrorResponse(w, "Invalid request format!!", http.StatusBadRequest)
-		return
-	}
-
-	svc := NewSpeakerDeckSvc(GetHTTPClient(ctx))
+func DownloadFromSpeakerDeck(ctx context.Context, w http.ResponseWriter, data *ReqData) {
+	svc := NewSpeakerDeckSvc(NewHTTPClient(ctx))
 	info, err := svc.GetSpeakerDeckInfo(data.URL)
 	if err != nil {
-		log.Debugf(ctx, "GetSpeakerDeckInfo error: %#v", err)
-		writeErrorResponse(w, "スライドが見つかりませんでした。", http.StatusNotFound)
+		log.Infof(ctx, "GetSpeakerDeckInfo error: %#v", err)
+		writeError(w, "スライドが見つかりませんでした。", http.StatusNotFound)
 		return
 	}
 
 	resp, err := download(ctx, info.DownloadURL)
 	if err != nil {
 		log.Errorf(ctx, "download error: %#v", err)
-		writeErrorResponse(w, "ダウンロード中にエラーが発生しました。", http.StatusInternalServerError)
+		writeError(w, "ダウンロード中にエラーが発生しました。", http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
-	w.Header().Set("Content-Disposition", "attachment; filename="+info.FileName)
-	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-	w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
-	w.Header().Set("X-FileName", info.FileName)
-	io.Copy(w, resp.Body)
+	copyResponseAsFile(w, resp, info.FileName)
 }
 
-var GetHTTPClient func(ctx context.Context) *http.Client = func(ctx context.Context) *http.Client {
+var NewHTTPClient func(ctx context.Context) *http.Client = func(ctx context.Context) *http.Client {
 	return urlfetch.Client(ctx)
 }
 
@@ -103,10 +115,18 @@ func json2ReqData(rc io.ReadCloser) (*ReqData, error) {
 
 func download(ctx context.Context, url string) (resp *http.Response, err error) {
 	ctxWithTimeout, _ := context.WithTimeout(ctx, 60*time.Second)
-	return GetHTTPClient(ctxWithTimeout).Get(url)
+	return NewHTTPClient(ctxWithTimeout).Get(url)
 }
 
-func writeErrorResponse(w http.ResponseWriter, message string, statusCode int) {
+func writeError(w http.ResponseWriter, message string, statusCode int) {
 	w.WriteHeader(statusCode)
 	_ = json.NewEncoder(w).Encode(ResError{Message: message})
+}
+
+func copyResponseAsFile(w http.ResponseWriter, resp *http.Response, fileName string) {
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
+	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
+	w.Header().Set("X-FileName", fileName)
+	io.Copy(w, resp.Body)
 }
--- server.go
+++ server.go
@@ -4,25 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"golang.org/x/net/context"
-	"google.golang.org/appengine"
 )
 
 func init() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/speakerdeck/download", httpWithContext(DownloadFromSpeakerDeck)).Methods("POST")
-	router.HandleFunc("/api/slideshare/download", httpWithContext(DownloadFromSlideShare)).Methods("POST")
+	router.HandleFunc("/api/speakerdeck/download", HttpWithContext(MustParams(DownloadFromSpeakerDeck))).Methods("POST")
+	router.HandleFunc("/api/slideshare/download", HttpWithContext(MustParams(DownloadFromSlideShare))).Methods("POST")
 	http.Handle("/", router)
 }
 
-type HandlerWithContext func(context.Context, http.ResponseWriter, *http.Request)
-
-func httpWithContext(h HandlerWithContext) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h(NewContext(r), w, r)
-	})
-}
-
-var NewContext func(*http.Request) context.Context = func(r *http.Request) context.Context {
-	return appengine.NewContext(r)
-}
