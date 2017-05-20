8676164713e8f127130e7583285d5ee061b1e8e4
Yoshitomo Minami <minami530@gmail.com>
2017-05-21 00:36:30 +0900

    Refactoring

--- server.go
+++ server.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
 	router := httprouter.New()
-	router.GET("/_ah/start", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {})
-	router.GET("/_ah/stop", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {})
+	router.GET("/_ah/start", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		log.Infof(NewContext(r), "START")
+	})
+	router.GET("/_ah/stop", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		log.Infof(NewContext(r), "STOP")
+	})
 	router.POST("/api/speakerdeck/download", HTTPWithContext(MustParams(DownloadFromSpeakerDeck)))
 	router.POST("/api/slideshare/download", HTTPWithContext(MustParams(DownloadFromSlideShare)))
 	http.Handle("/", router)
